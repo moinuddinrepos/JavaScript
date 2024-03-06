481568708349b8cd7cba2110c542ba175266f8b4
MD Khaja Moinuddin <88581014+moinuddinrepos@users.noreply.github.com>
2024-03-06 14:32:55 +0000

    commit

---
+++ 01_Basics/03_Conversions&Operations.js
@@ -0,0 +1,38 @@
+// // let str = 'Moinuddin' //if we concver into Number ---> NaN
+// let myNum = "99abc"
+// let convNum = Number(myNum)
+// let imHandsome = true
+// let convimHandsome = Number(imHandsome)
+// // let convStr = Number(str);
+// console.log(convNum);
+// console.log(typeof convNum)
+
+// /////////***************Opearions */
+// let str1 = "Md Khaja"
+// let str2 = " Moinuddin"
+// let strAdd = str1 + str2
+// console.log(strAdd)
+
+// console.log(2+2);
+// console.log("2" + 2);
+// console.log(2 + "2");
+// console.log(2 + 2 + 3);
+// console.log((2+2)+"2");
+// console.log(true)
+// console.log(+true)
+
+// let gameCounter = 50
+// ++gameCounter
+// console.log(gameCounter);
+
+
+
+
+let x = 3
+const y = x++
+console.log(`x: ${x}, y: ${y}`)
+
+let a = 3
+const b = ++a
+console.log(`a:${a}, b:${b}`)
+
