744608a547cebf4e9071ecebb9d5649aabff0f5a
MD Khaja Moinuddin <88581014+moinuddinrepos@users.noreply.github.com>
2024-03-06 12:43:08 +0000

    commit

--- 01_Basics/Variables.js
+++ 01_Basics/Variables.js
@@ -1,6 +1,7 @@
 const studentId = '18K95A0512' //we can't chnage value once we declared in const variable.
 let studentName = "Md Khaja Moinuddin" //we can change
 var studentCity = "Hyderabad" //we can use this variable but we face issues in scope functions
+let studentFname
 studentPhoneno = 9876543210 // we can declare variable without variable name but its wrong method.
 
 // studentId = '18jhyer155' // ex: we cant change value once we declared in const
@@ -8,4 +9,4 @@ studentPhoneno = 9876543210 // we can declare variable without variable name but
 studentName = 'Nizamuddin'
 studentCity = 'Bangaluru'
 
-console.table([studentId, studentName, studentCity, studentPhoneno]);
+console.table([studentId, studentFname, studentName, studentCity, studentPhoneno]);
---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,9 @@
+const myName = "Moinuddin";
+let age = 26;
+let inHome = true;
+
+
+//Types of DataTypes
+//String, num, boolean, bigint, null(null is typeof object), undefined, symbol, objects.
+
+console.log(typeof null);
