b38592f9b5c851e2b0e05b3e71975f3923dc17fe
MD Khaja Moinuddin <88581014+moinuddinrepos@users.noreply.github.com>
2024-03-06 15:27:29 +0000

    commit

---
+++ 01_Basics/StackandHeep.js
@@ -0,0 +1,23 @@
+// Stack(primitive data type), heep (Non-primitive datatype)
+let myName = 'Moinuddin'
+let anotherName = myName
+console.log(anotherName);
+console.log(myName)
+
+
+let userOne = {
+    name: 'moinuddin',
+    age: 26,
+    email: "moin@gmail.com"
+}
+
+let userTwo = userOne
+userTwo.email = 'anser@gmail.com'
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+
+
+
+
