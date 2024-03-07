f6666b724de79b240e77f7d10274519ce1368b22
MD Khaja Moinuddin <88581014+moinuddinrepos@users.noreply.github.com>
2024-03-07 14:30:53 +0000

    Ayyar commit

--- 02_basics/Arrays.js
+++ 02_basics/Arrays.js
@@ -1,35 +1,35 @@
-// const myArray = ['a','b','c']
+const myArray = ['a','b','c']
 
-// console.log(myArray)
+console.log(myArray)
 
-// Array = ['a','b','c']
-// console.log(Array.length);
+Array = ['a','b','c']
+console.log(Array.length);
 
-// //Array methods
+//Array methods
 
-// myArray.push('d')
-// myArray.push('e')
-// myArray.push('f')
-// myArray.unshift('z')
-// console.log(myArray);
-// myArray.pop() //no need to pass argument last element will be removed
+myArray.push('d')
+myArray.push('e')
+myArray.push('f')
+myArray.unshift('z')
+console.log(myArray);
+myArray.pop() //no need to pass argument last element will be removed
 
-// console.log(myArray);
+console.log(myArray);
 
-// const newArray = myArray.join()
-// console.log(typeof myArray);
-// console.log(typeof newArray);
+const newArray = myArray.join()
+console.log(typeof myArray);
+console.log(typeof newArray);
 
 
-// const OraginalArr = ['a','b','c','d','e']
-// console.log("A",  OraginalArr)
+const OraginalArr = ['a','b','c','d','e']
+console.log("A",  OraginalArr)
 
-// const sliceArr = ['a','b','c','d','e']
-// console.log("B", sliceArr.slice(1,4));
+const sliceArr = ['a','b','c','d','e']
+console.log("B", sliceArr.slice(1,4));
 
-// const spliceArr = ['a','b','c','d','e']
-// console.log("C", spliceArr.splice(1,4));
-// console.log(spliceArr);
+const spliceArr = ['a','b','c','d','e']
+console.log("C", spliceArr.splice(1,4));
+console.log(spliceArr);
 
 
 
@@ -42,25 +42,25 @@ console.log(indiaCity);
 const USACity = ['LA','NY','WDC','SF'] 
 console.log(USACity);
 
-// indiaCity.push(USACity)
-// console.log(indiaCity);
+indiaCity.push(USACity)
+console.log(indiaCity);
 
-// const grpCity = (indiaCity + USACity)
-// console.log(grpCity);
+const grpCity = (indiaCity + USACity)
+console.log(grpCity);
 
-// const grpCity = indiaCity.concat(USACity)
-// console.log(grpCity);
-
-
-// const spritArr = [...indiaCity, ...USACity]
-// console.log(spritArr);
+const grpCity = indiaCity.concat(USACity)
+console.log(grpCity);
 
 
-// const flatArr = [indiaCity, USACity]
-// console.log(flatArr.flat(Infinity));
+const spritArr = [...indiaCity, ...USACity]
+console.log(spritArr);
 
-// const subArr = ['a','b','c',['d','a'],'e','f',['g',['h','i']]]
-// console.log(subArr.flat(1));
+
+const flatArr = [indiaCity, USACity]
+console.log(flatArr.flat(Infinity));
+
+const subArr = ['a','b','c',['d','a'],'e','f',['g',['h','i']]]
+console.log(subArr.flat(1));
 
 const convArr = ('Moinuddin')
 console.log(Array.isArray(convArr));
