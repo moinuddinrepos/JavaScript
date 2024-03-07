fbcda7886467943be8d283acade6a493687bd268
MD Khaja Moinuddin <88581014+moinuddinrepos@users.noreply.github.com>
2024-03-07 19:13:44 +0000

    commit obj 4

---
+++ 02_basics/object_3.js
@@ -0,0 +1,12 @@
+// de structure objects
+
+
+const stu = {
+    stuId: 1234,
+    stuName: 'Moin',
+    stuPlace: 'Hyderabad',
+    stuLogin: false
+}
+
+const {stuName: sname} = stu
+console.log(sname);
