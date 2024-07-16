12606bd6d39ea5cc30ed098a143a5a280b997444
MD Khaja Moinuddin <88581014+moinuddinrepos@users.noreply.github.com>
2024-07-16 16:21:24 +0000

    day4 challange js

---
+++ 30DaysChallange/day4loops.js
@@ -0,0 +1,25 @@
+//Activity Loop
+//Task-1 Write a program to print 1 - 10 using loop.
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+//Task-2 Write a program to print multiplicatiion table of 5using for loop.
+for (let i = 1; i <= 10; i++) {
+    console.log(`5 X ${i} = `,5*i) 
+    
+}
+
+
+//Activity 2 While loop
+//Task-3 Write a program to calculate the sum of 1 - 10 using while loop.
+
+let sum = 0
+let i = 1
+while (i <= 10) {
+    sum += i
+    i++;
+}
+console.log(sum)
+
+  
